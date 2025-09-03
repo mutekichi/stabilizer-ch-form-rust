c1dbaa361549f3c76fd31e521d6a99da0cce3866
Mutekichi <ppwatanabe19568@gmail.com>
2025-09-03 16:54:02 +0900

    debug bool array printer

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,4 +1,4 @@
-use ndarray::Array1;
+use ndarray::{Array1, Array2};
 use num_complex::Complex64;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -20,6 +20,7 @@ pub fn assert_eq_complex_array1(a: &Array1<Complex64>, b: &Array1<Complex64>) {
     }
 }
 
+#[allow(dead_code)]
 pub fn load_statevector_from_file<P: AsRef<Path>>(path: P) -> Result<Array1<Complex64>, std::io::Error> {
     let file = File::open(path)?;
     let reader = BufReader::new(file);
@@ -35,4 +36,22 @@ pub fn load_statevector_from_file<P: AsRef<Path>>(path: P) -> Result<Array1<Comp
         }
     }
     Ok(Array1::from(vec_data))
+}
+
+/// Prints a boolean vector (Array1<bool>) to the console in a readable format (e.g., [0, 1, 0]).
+#[allow(dead_code)]
+pub fn pretty_print_vec(name: &str, vec: &Array1<bool>) {
+    let s: String = vec.iter().map(|&b| if b { '1' } else { '0' }).collect::<String>();
+    println!("{}: [{}]", name, s);
+}
+
+/// Prints a boolean matrix (Array2<bool>) to the console in a readable format.
+#[allow(dead_code)] // This is a debug utility, so allow it to be unused in some tests
+pub fn pretty_print_mat(name: &str, mat: &Array2<bool>) {
+    println!("{}: [", name);
+    for row in mat.rows() {
+        let s: String = row.iter().map(|&b| if b { '1' } else { '0' }).collect();
+        println!("  {}", s);
+    }
+    println!("]");
 }
--- tests/gate_api.rs
+++ tests/gate_api.rs
@@ -32,3 +32,17 @@ fn test_bell_state() {
     ];
     common::assert_eq_complex_array1(&statevec, &expected);
 }
+
+#[test]
+fn test_hadamard_behaviour() {
+    let mut ch_form = StabilizerCHForm::new(1);
+    ch_form.apply_h(0);
+
+    let statevec = ch_form.to_statevector();
+    println!("Statevector after H: {:?}", statevec);
+    let expected = ndarray::array![
+        num_complex::Complex64::new(1.0 / 2f64.sqrt(), 0.0),
+        num_complex::Complex64::new(1.0 / 2f64.sqrt(), 0.0)
+    ];
+    common::assert_eq_complex_array1(&statevec, &expected);
+}
